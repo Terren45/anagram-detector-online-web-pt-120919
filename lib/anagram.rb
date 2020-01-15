6ff5fe0afa6e2a4498b5ebd309c1aed4031a072b
Terren Culberson <culbersonterren@yahoo.com>
2020-01-15 03:48:47 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,7 @@ class Anagram
     end
    
     def match(arr)
-     arr.keep_if { |str| name.split("").sort str.sort("") }
+     arr.keep_if { |str| name.split("").sort == str.sort("") }
     end
   end
   # Your code goes here!
