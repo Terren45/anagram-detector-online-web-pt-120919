a1bb5a273895e92ea6a82d8a24b108326c633ea6
Terren Culberson <culbersonterren@yahoo.com>
2020-01-15 02:49:19 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,10 +1,12 @@
 class Anagram
   
-  attr_accessor :name
-   def initialize(name)
-    @name = name
-   end
+   attr_accessor :name
+    def initialize(name)
+     @name = name
+    end
    
-   def match(string)
+    def match(string)
  
-  end  # Your code goes here!
+    end
+  end
+  # Your code goes here!
